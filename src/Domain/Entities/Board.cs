8b20875200787aaabab2b66fdf00024ce921ea36
Bartłomiej Stefański <62449513+VarytDev@users.noreply.github.com>
2024-03-26 19:58:34 +0100

    Added entity classes

---
+++ src/Domain/Entities/Board.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskManagementApp.Domain.Entities;
+internal class Board
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public ICollection<TaskList> TaskLists { get; set; } = new List<TaskList>();
+}
---
+++ src/Domain/Entities/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagementApp.Domain.Entities;
+internal class Task
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public int TaskListId { get; set; }
+    public TaskList List { get; set; } = null!;
+}
